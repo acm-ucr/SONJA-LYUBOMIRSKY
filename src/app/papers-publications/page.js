7f192d62134937692f6ecb3cecd3801d2208828c
menthy-wu <wumengxuan0728@gmail.com>
2024-01-28 14:41:02 -0800

    header and button

---
+++ src/app/papers-publications/page.js
@@ -0,0 +1,17 @@
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+
+const page = () => {
+  return (
+    <div>
+      <Header text="Papers & Publications" />
+      <Button
+        link="https://scholar.google.com/citations?user=CNFj4ZsAAAAJ&hl=en"
+        text="Selected Publications (with PDF)"
+      />
+      page
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({ text }) => {
+  return (
+    <div className="text-6xl text-sonja-orange-200 font-cormorant font-bold">
+      {text}
+    </div>
+  );
+};
+
+export default Header;
