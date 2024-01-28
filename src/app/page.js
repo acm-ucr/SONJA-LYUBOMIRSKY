680c9df1f65d6886c81cc23963361dd07b7de609
menthy-wu <wumengxuan0728@gmail.com>
2024-01-28 14:09:35 -0800

    set up tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <>Hello World</>;
+  return <div className="">Hello World</div>;
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cormorant: ["var(--font-cormorant)"],
+        montserrat: ["var(--font-montserrat)"],
+      },
+      colors: {
+        sonja: {
+          black: "#1E1E1E",
+          "orange-100": "#F6ECE6",
+          "orange-200": "#FF6600",
+          blue: "#1E598F",
+          gray: "#F7F7F7",
+        },
+      },
+    },
   },
   plugins: [],
 };
