88d0a19dd137d57ee0b60942ab87cf5e175ea358
menthy-wu <wumengxuan0728@gmail.com>
2024-01-28 14:47:46 -0800

    add images

--- src/app/papers-publications/page.js
+++ src/app/papers-publications/page.js
@@ -1,14 +1,17 @@
 import Button from "@/components/Button";
 import Header from "@/components/Header";
+import Image from "next/image";
+import header from "@/public/Publications.JPG";
 
 const page = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <Header text="Papers & Publications" />
       <Button
         link="https://scholar.google.com/citations?user=CNFj4ZsAAAAJ&hl=en"
         text="Selected Publications (with PDF)"
       />
+      <Image src={header} alt="publicatoins" />
       page
     </div>
   );
