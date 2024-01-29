a322b1816bd97eb6a2eb90af5e8ebc5d646f2081
menthy-wu <wumengxuan0728@gmail.com>
2024-01-29 13:24:38 -0800

    lab

--- src/data/Documents/Sections.js
+++ src/data/Documents/Sections.js
@@ -3,6 +3,8 @@ import { BROADCAST } from "./Broadcast";
 import { PAPERS } from "./Papers";
 import { POPULAR_PRESS } from "./PopularPress";
 import { PRINTS } from "./Print";
+import { RECENT_PHD } from "./RecentPhd";
+import { RESEARCH } from "./Research";
 import { GrDocumentText } from "react-icons/gr";
 import { IoBookOutline } from "react-icons/io5";
 import { ImNewspaper } from "react-icons/im";
@@ -27,7 +29,8 @@ export const SECTIONS = {
     { title: "Past & Future Talks", items: PRINTS, icon: <FiMic /> },
   ],
   "PAW Lab": [
-    { title: "Lab", items: PRINTS },
+    { title: "Most Recent PHDs", items: RECENT_PHD },
+    { title: "Lab", items: RESEARCH },
     { title: "Get Involved", items: PRINTS },
   ],
 };
@@ -48,9 +51,5 @@ export const HEADER = {
       text: "To schedule an interview please write to sonja@ucr.edu",
       link: "mailto:sonja@ucr.edu",
     },
-  ],
-  lab: [
-    { title: "Most Recent PHDs", items: RECENT_PHD },
-    { title: "Current Research Projects", items: RESEARCH },
   ],
 };
