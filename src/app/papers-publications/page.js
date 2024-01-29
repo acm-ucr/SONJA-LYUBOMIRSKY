4eb77ed6ed38c753b645087dc9a369fccbbb9f8f
menthy-wu <wumengxuan0728@gmail.com>
2024-01-28 20:44:51 -0800

    add button

--- src/app/papers-publications/page.js
+++ src/app/papers-publications/page.js
@@ -10,16 +10,20 @@ const page = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="bg-sonja-orange-100 w-full flex justify-evenly items-center">
-        <div className="flex flex-col items-end">
+        <div className="flex flex-col items-end gap-2">
           <Header text="Papers & Publications" />
           <Button
             link="https://scholar.google.com/citations?user=CNFj4ZsAAAAJ&hl=en"
             text="Selected Publications (with PDF)"
           />
+          <Button
+            link="http://sonjalyubomirsky.com/subjective-happiness-scale-shs"
+            text="Subjective Happiness Scale"
+          />
         </div>
         <Image src={header} alt="publicatoins" className="w-1/4" />
       </div>
-      <div className="w-9/12 flex m-5 justify-evenly">
+      <div className="w-9/12 flex my-8 justify-evenly">
         <div className="w-8/12">
           {SECTIONS.map((section, index) => {
             return (
